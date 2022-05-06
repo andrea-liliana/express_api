08951677815f5064219cd784c23825289649777d
andrea-liliana <andrea.mx1998@gmail.com>
2022-05-07 00:26:33 +0200

    Agregar Express API

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.listen(port, () => {
 })
 
 // HTTP METHODS
-//GET - Creación de endpoint que regresa una lista
+// GET - Creación de endpoint que regresa una lista
 app.get('/v1/explorers', (req, res) => {
     console.log(`Api Explorers GET ALL requests ${new Date()}`)
     const explorer1 = {id: 1, name: "Andrea1"}
@@ -22,3 +22,35 @@ app.get('/v1/explorers', (req, res) => {
     const explorers = [explorer1, explorer2, explorer3, explorer4]
     res.status(200).json(explorers)
 }) 
+
+// GET - Creación de endpoint que regresa un objeto
+app.get('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers GET request ${new Date()}`)
+    console.log(`Getting explorer with id ${req.params.id}`)
+    const explorer = {id:1, name: "Andrea"}
+    res.status(200).json(explorer)
+})
+
+// POST - Creación de endpoint que se encarga de crear un objeto 
+app.post('/v1/explorers', (req, res) => {
+    console.log(`Api Explorers POST request ${new Date()}`)
+    const requestBody = req.body // Parámetros de un cliente
+    console.log(requestBody)
+    res.status(201).json({message: "Created"})
+})
+
+// PUT - Creación de un endpoint que se encargue de actualizar un objeto
+app.put('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers PUT request ${new Date()}`)
+    console.log(`Update explorer with id ${req.params.id}`)
+    const requestBody = req.body // Parámetros de un cliente
+    res.status(200).json({message: "Updated!"})
+})
+
+// DELETE - Creación de un endpoint para eliminar un objeto
+app.delete('/v1/explorers/:id', (req, res) => {
+    console.log(`Api Explorers DELETE request ${new Date()}`)
+    console.log(`Delete explorer with id ${req.params.id}`)
+    const requestBody = req.body // Parámetros de un cliente
+    res.status(200).json({message: "Deleted"})
+})
